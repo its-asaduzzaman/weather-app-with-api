35879768bf6950a0c6bc082d81783f74616e787f
AsaduzzamanSam <samrat.asad@gmail.com>
2021-11-12 09:11:05 +0600

    work with initsate method , try catch exception concept, flutter http pacakge, weather api with personal api key

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:weather/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,30 +8,39 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getLocation()async {
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    print(position);
-
-  }
-
   @override
   void initState() {
-    getLocation();
     // TODO: implement initState
     super.initState();
+    getLocation();
   }
 
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.longitude);
+    print(location.latitude);
+  }
 
+  void getData() async {
+    http.Response response = await http.get(Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=7904a67856d292ac96f26e994effae02'));
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
       body: Center(
         child: ElevatedButton(
           onPressed: () {
             // getLocation();
-
-            
           },
           child: Text('Get Location'),
         ),
